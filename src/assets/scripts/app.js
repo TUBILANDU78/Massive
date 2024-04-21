b3c89e7f2a3ce66eef95544cf14f0c58cddc9bad
Rubén Martín Bravo <rmartinbra@uoc.edu>
2024-04-21 14:12:32 +0200

    Final commit

---
+++ src/assets/scripts/app.js
@@ -0,0 +1,20 @@
+AOS.init({
+  duration: 1200,
+})
+
+
+document.getElementById("newsletterForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var email = document.getElementById("email").value;
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Por favor, introduce un correo electrónico válido.");
+        return;
+    }
+
+    alert("Correo electrónico enviado a: " + email);
+
+    document.getElementById("email").value = '';
+});
